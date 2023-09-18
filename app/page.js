a9a27349939efd031a587c8b54f0da9b98c5a9ff
Wojtek <wwojciechjarocki@gmail.com>
2023-09-18 09:24:53 +0200

    apollo client config

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
 import Image from 'next/image'
 
+async function getData() {
+  
+}
+
+
+
+
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -7,3 +16,25 @@ export default function Home() {
     </main>
   )
 }
+
+
+
+// query Offer {
+//   offer(where:{id:"clmoj9yhnsyrs0aw6ffgn3ssf"}) {
+//     amenities
+//     createdAt
+//     id
+//     images {
+//       url
+//     }
+//     location {
+//       latitude
+//       longitude
+//     }
+//     title
+//     stars
+//     description {
+//       html
+//     }
+//   }
+// }
