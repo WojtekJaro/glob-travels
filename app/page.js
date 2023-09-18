11ddd0f7d437afb5ddb4bcc425dfd4d5b64b5cdc
Wojtek <wwojciechjarocki@gmail.com>
2023-09-18 10:48:11 +0200

    gql connection

--- app/page.js
+++ app/page.js
@@ -1,23 +1,22 @@
-import Image from 'next/image'
+import { GET_ALL_TRAVELS } from '@/gql/getAllTravels'
+import { cmsConnect } from '@/utils/cmsConnector'
+import Link from 'next/link';
 
-async function getData() {
-  
+export async function getData() {
+	const data = await cmsConnect(GET_ALL_TRAVELS)
+	return data;
 }
+export default async function Home() {
+  const {offers} = await getData()
+  console.log(offers)
 
+	return (
+  <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    {offers.map(offer => <Link key={offers.id} href={`/oferta/${offer.slug}`}> {offer.title} </Link>)}
 
-
-
-
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+  </main>
   )
 }
-
-
 
 // query Offer {
 //   offer(where:{id:"clmoj9yhnsyrs0aw6ffgn3ssf"}) {
